d4b2edafd751f6c3ff94c071df459c23e610d70b
mottox2 <y.mottox2@gmail.com>
2019-04-19 02:17:10 +0900

    Change structure

--- src/actions.ts
+++ src/actions.ts
@@ -25,26 +25,25 @@ function getActionType(text: TriggerKeys) {
 function doAction(event: any, payload: any) {
   const now = Moment.moment().format('YYYY/MM/DD HH:mm')
   const message = payload.userName + ': ' + now
+  const lastRecord = getLastRecord(payload.userName)
+  Logger.log(lastRecord.row)
   switch (event) {
     case START_EVENT:
-      addRecord(payload.userName, {
-        eventName: START_EVENT,
-        stampedAt: now
-      })
+      if (!lastRecord.isComplete()) {
+        postToSlack(`前回の完了記録が見つかりません。`)
+        break
+      }
+      const newRecord = new TimeRecord(lastRecord.row + 1, now, '')
+      updateRecord(payload.userName, newRecord)
       postToSlack('[START] ' + message)
       break
     case END_EVENT:
-      const lastRecord = getLastRecord(payload.userName)
-      if (lastRecord.eventName !== START_EVENT) {
-        postToSlack(payload.userName + 'さんの開始記録がみつかりませんでした')
-        return
+      if (lastRecord.isComplete()) {
+        postToSlack(`開始記録が見つかりませんでした`)
+        break
       }
-      const lastRow = lastRecord.row
-      addRecord(payload.userName, {
-        eventName: END_EVENT,
-        stampedAt: now,
-        result: '=B' + (lastRow + 1) + '-B' + lastRow
-      })
+      lastRecord.endedAt = now
+      updateRecord(payload.userName, lastRecord)
       postToSlack('[END] ' + message)
       break
     case PING_EVENT:
