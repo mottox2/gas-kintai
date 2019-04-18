ca473f501b2d7c33e556c63d09c92bd64acbc7bc
mottox2 <y.mottox2@gmail.com>
2019-04-19 00:28:27 +0900

    Fix

--- src/sheet.ts
+++ src/sheet.ts
@@ -1,11 +1,12 @@
-var columns = [
-  'eventName', 'stampedAt', 'result'
-]
+var columns = ['eventName', 'stampedAt', 'result']
+
+function getSpreadsheet(sheetId?: string | null): GoogleAppsScript.Spreadsheet.Spreadsheet {
+  return sheetId ? SpreadsheetApp.openById(sheetId) : SpreadsheetApp.getActive()
+}
 
 function createSheetByName(name: string) {
   const properties = getProperties()
-  if (properties.SHEET_ID == null) return
-  const spreadsheet = SpreadsheetApp.openById(properties.SHEET_ID);
+  const spreadsheet = getSpreadsheet(properties.SHEET_ID)
   const newSheet = spreadsheet.insertSheet()
   newSheet.setName(name)
   newSheet.appendRow(columns)
@@ -14,8 +15,7 @@ function createSheetByName(name: string) {
 
 function findOrCreateSheetByName(name: string) {
   const properties = getProperties()
-  if (properties.SHEET_ID == null) return
-  const spreadsheet = SpreadsheetApp.openById(properties.SHEET_ID);
+  const spreadsheet = getSpreadsheet(properties.SHEET_ID)
   const sheet = spreadsheet.getSheetByName(name)
   if (sheet) {
     return sheet
